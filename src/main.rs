fa22e9d1e2b3f34b0fed4ef41858a54eb02e0411
Vince <vincivenv@gmail.com>
2023-03-25 23:51:50 +0100

    somewhere

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use bindo::{compress, decompress};
+
+    #[test]
+    fn compress_and_decompress_works() {
+        // let input = include_str!("../random.txt").as_bytes();
+
+        let input = b"hello world!";
+        let compressed = compress(input, input.len());
+        let decompressed = decompress(&compressed, input.len());
+        println!("{:?}", decompressed);
+        assert_eq!(decompressed, input);
+    }
+
+    // #[test]
+    // #[should_panic(expected = "BZ_PARAM_ERROR")]
+    // fn compress_handles_bad_parameter() {
+    //     let input = vec![0; 100];
+    //     let compressed = compress(&input[..]);
+    // }
+
+    // #[test]
+    // #[should_panic(expected = "BZ_MEM_ERROR")]
+    // fn compress_handles_memory_error() {
+    //     let input = vec![0; 1000000000];
+    //     let compressed = compress(&input[..]);
+    // }
+
+    // #[test]
+    // #[should_panic(expected = "BZ_CONFIG_ERROR")]
+    // fn decompress_handles_config_error() {
+    //     let compressed = vec![0; 100];
+    //     let decompressed = decompress(&compressed[..], 1000);
+    // }
+
+    // #[test]
+    // #[should_panic(expected = "BZ_PARAM_ERROR")]
+    // fn decompress_handles_bad_parameter() {
+    //     let compressed = vec![0; 100];
+    //     let decompressed = decompress(&compressed[..], 0);
+    // }
+
+    // #[test]
+    // #[should_panic(expected = "BZ_MEM_ERROR")]
+    // fn decompress_handles_memory_error() {
+    //     let compressed = vec![0; 1000000000];
+    //     let decompressed = decompress(&compressed[..], 1000);
+    // }
 }
