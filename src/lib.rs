f4eb3d7f6972a96810f748fcc83da17526beec98
Vince <vincivenv@gmail.com>
2023-03-26 01:00:45 +0100

    Some ref

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use std::mem;
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 pub fn compress(input: &[u8]) -> Vec<u8> {
     unsafe {
-        let mut compressed_output: Vec<u8> = vec![0; input.len()];
+        let mut compressed_out: Vec<u8> = vec![0; input.len()];
 
         // Construct a compression stream.
         let mut stream: bz_stream = mem::zeroed();
@@ -17,43 +17,26 @@ pub fn compress(input: &[u8]) -> Vec<u8> {
             4, // verbosity (4 = most verbose)
             0,
         ); // default work factor
-        match result {
-            r if r == (BZ_CONFIG_ERROR as _) => panic!("BZ_CONFIG_ERROR"),
-            r if r == (BZ_PARAM_ERROR as _) => panic!("BZ_PARAM_ERROR"),
-            r if r == (BZ_MEM_ERROR as _) => panic!("BZ_MEM_ERROR"),
-            r if r == (BZ_OK as _) => {}
-            r => panic!("Unknown return value = {}", r),
-        }
+        handle_bz_error(result, "BZ2_bzCompressInit");
 
-        // Compress `input` into `compressed_output`.
+        // Compress `input` into `compressed_out`.
         stream.next_in = input.as_ptr() as *mut _;
         stream.avail_in = input.len() as _;
-        stream.next_out = compressed_output.as_mut_ptr() as *mut _;
-        stream.avail_out = compressed_output.len() as _;
+        stream.next_out = compressed_out.as_mut_ptr() as *mut _;
+        stream.avail_out = compressed_out.len() as _;
         let result = BZ2_bzCompress(&mut stream as *mut _, BZ_FINISH as _);
-        match result {
-            r if r == (BZ_RUN_OK as _) => panic!("BZ_RUN_OK"),
-            r if r == (BZ_FLUSH_OK as _) => panic!("BZ_FLUSH_OK"),
-            r if r == (BZ_FINISH_OK as _) => panic!("BZ_FINISH_OK"),
-            r if r == (BZ_SEQUENCE_ERROR as _) => panic!("BZ_SEQUENCE_ERROR"),
-            r if r == (BZ_STREAM_END as _) => {}
-            r => panic!("Unknown return value = {}", r),
-        }
+        handle_bz_error(result, "BZ2_bzCompress");
 
         // Finish the compression stream.
         let result = BZ2_bzCompressEnd(&mut stream as *mut _);
-        match result {
-            r if r == (BZ_PARAM_ERROR as _) => panic!("BZ_PARAM_ERROR"),
-            r if r == (BZ_OK as _) => {}
-            r => panic!("Unknown return value = {}", r),
-        }
-        compressed_output
+        handle_bz_error(result, "BZ2_bzCompressEnd");
+        compressed_out
     }
 }
 
-pub fn decompress(compressed_output: Vec<u8>, comp_len: usize) -> Vec<u8> {
+pub fn decompress(compressed_out: Vec<u8>, comp_len: usize) -> Vec<u8> {
     unsafe {
-        let mut decompressed_output: Vec<u8> = vec![0; comp_len];
+        let mut decompressed_out: Vec<u8> = vec![0; comp_len];
         // Construct a decompression stream.
         let mut stream: bz_stream = mem::zeroed();
         let result = BZ2_bzDecompressInit(
@@ -61,37 +44,36 @@ pub fn decompress(compressed_output: Vec<u8>, comp_len: usize) -> Vec<u8> {
             4, // verbosity (4 = most verbose)
             0,
         ); // default small factor
-        match result {
-            r if r == (BZ_CONFIG_ERROR as _) => panic!("BZ_CONFIG_ERROR"),
-            r if r == (BZ_PARAM_ERROR as _) => panic!("BZ_PARAM_ERROR"),
-            r if r == (BZ_MEM_ERROR as _) => panic!("BZ_MEM_ERROR"),
-            r if r == (BZ_OK as _) => {}
-            r => panic!("Unknown return value = {}", r),
-        }
+        handle_bz_error(result, "BZ2_bzDecompressInit");
 
-        // Decompress `compressed_output` into `decompressed_output`.
-        stream.next_in = compressed_output.as_ptr() as *mut _;
-        stream.avail_in = compressed_output.len() as _;
-        stream.next_out = decompressed_output.as_mut_ptr() as *mut _;
-        stream.avail_out = decompressed_output.len() as _;
+        // Decompress `compressed_out` into `decompressed_out`.
+        stream.next_in = compressed_out.as_ptr() as *mut _;
+        stream.avail_in = compressed_out.len() as _;
+        stream.next_out = decompressed_out.as_mut_ptr() as *mut _;
+        stream.avail_out = decompressed_out.len() as _;
         let result = BZ2_bzDecompress(&mut stream as *mut _);
-        match result {
-            r if r == (BZ_PARAM_ERROR as _) => panic!("BZ_PARAM_ERROR"),
-            r if r == (BZ_DATA_ERROR as _) => panic!("BZ_DATA_ERROR"),
-            r if r == (BZ_DATA_ERROR_MAGIC as _) => panic!("BZ_DATA_ERROR"),
-            r if r == (BZ_MEM_ERROR as _) => panic!("BZ_MEM_ERROR"),
-            r if r == (BZ_OK as _) => panic!("BZ_OK"),
-            r if r == (BZ_STREAM_END as _) => {}
-            r => panic!("Unknown return value = {}", r),
-        }
-
+        handle_bz_error(result, "BZ2_bzDecompress");
+        
         // Close the decompression stream.
         let result = BZ2_bzDecompressEnd(&mut stream as *mut _);
-        match result {
-            r if r == (BZ_PARAM_ERROR as _) => panic!("BZ_PARAM_ERROR"),
-            r if r == (BZ_OK as _) => {}
-            r => panic!("Unknown return value = {}", r),
-        }
-        decompressed_output
+        handle_bz_error(result, "BZ2_bzDecompressEnd");
+        decompressed_out
+    }
+}
+
+fn handle_bz_error(result: i32, name: &str) {
+    match result {
+        r if r == (BZ_CONFIG_ERROR as _) => panic!("{}: BZ_CONFIG_ERROR", name),
+        r if r == (BZ_PARAM_ERROR as _) => panic!("{}: BZ_PARAM_ERROR", name),
+        r if r == (BZ_MEM_ERROR as _) => panic!("{}: BZ_MEM_ERROR", name),
+        r if r == (BZ_OK as _) => {}
+        r if r == (BZ_RUN_OK as _) => panic!("BZ_RUN_OK"),
+        r if r == (BZ_FLUSH_OK as _) => panic!("BZ_FLUSH_OK"),
+        r if r == (BZ_FINISH_OK as _) => panic!("BZ_FINISH_OK"),
+        r if r == (BZ_SEQUENCE_ERROR as _) => panic!("BZ_SEQUENCE_ERROR"),
+        r if r == (BZ_STREAM_END as _) => {}
+        r if r == (BZ_DATA_ERROR as _) => panic!("BZ_DATA_ERROR"),
+        r if r == (BZ_DATA_ERROR_MAGIC as _) => panic!("BZ_DATA_ERROR"),
+        r => panic!("{}: Unknown return value = {}", name, r),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,11 @@ mod tests {
     #[test]
     fn compress_and_decompress_works() {
         let input = include_str!("../random.txt").as_bytes();
-        let compressed_output = compress(input);
-        let decompressed_output = decompress(compressed_output, input.len());
-        assert_eq!(input, &decompressed_output[..]);
+        // let input = "hello world".as_bytes();
+
+        let compressed_out = compress(input);
+        let decompressed_out = decompress(compressed_out, input.len());
+        println!("{:?}, \n {:?}", input, decompressed_out);
+        assert_eq!(input, &decompressed_out[..]);
     }
-
-    // #[test]
-    // #[should_panic(expected = "BZ_PARAM_ERROR")]
-    // fn compress_handles_bad_parameter() {
-    //     let input = vec![0; 100];
-    //     let compressed = compress(&input[..]);
-    // }
-
-    // #[test]
-    // #[should_panic(expected = "BZ_MEM_ERROR")]
-    // fn compress_handles_memory_error() {
-    //     let input = vec![0; 1000000000];
-    //     let compressed = compress(&input[..]);
-    // }
-
-    // #[test]
-    // #[should_panic(expected = "BZ_CONFIG_ERROR")]
-    // fn decompress_handles_config_error() {
-    //     let compressed = vec![0; 100];
-    //     let decompressed = decompress(&compressed[..], 1000);
-    // }
-
-    // #[test]
-    // #[should_panic(expected = "BZ_PARAM_ERROR")]
-    // fn decompress_handles_bad_parameter() {
-    //     let compressed = vec![0; 100];
-    //     let decompressed = decompress(&compressed[..], 0);
-    // }
-
-    // #[test]
-    // #[should_panic(expected = "BZ_MEM_ERROR")]
-    // fn decompress_handles_memory_error() {
-    //     let compressed = vec![0; 1000000000];
-    //     let decompressed = decompress(&compressed[..], 1000);
-    // }
 }
